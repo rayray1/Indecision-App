fb220eded24c75868dd9e0378ae9f48d2d69dbca
Raymond Rupiah <rupiray@yahoo.com>
2017-11-13 06:50:32 +0000

    new files added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,14 @@
+'use strict';
+
 console.log('App.js is running');
 
 //  JSX - Javascript XML
-// var template = <p>This is JSX from app.js!</p>;
 var template = React.createElement(
-    "h2",
-    { id: "someid" },
-    "Something else"
+  'h1',
+  null,
+  'what is this?'
 );
+
 var appRoot = document.getElementById('app');
 
-ReactDom.render(template, appRoot);
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('App.js is running');
+
+//  JSX - Javascript XML
+var template = <h1>what is this?</h1>;
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
