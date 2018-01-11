ddefcd5420dc08560deefb986d0ff39ec346e275
Raymond Rupiah <rupiray@yahoo.com>
2018-01-11 03:19:09 +0000

    new files added

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -96,15 +96,17 @@ export default class IndecisionApp extends React.Component {
                         handlePick={this.handlePick} // passing the function as a prop
 
                     />
-                    <Options
-                        options={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption}
-                    />
-                    <AddOption
-                        handleAddOption={this.handleAddOption}
+                    <div className="widget">
+                        <Options
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption
+                            handleAddOption={this.handleAddOption}
 
-                    />
+                        />
+                    </div>
                 </div>
                 <OptionModal 
                 selectedOption={this.state.selectedOption}
--- src/components/Option.js
+++ src/components/Option.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 // Stateless Option component - Implicitly returning jsx(not using return keyword)
 const Option = (props) => (
-    <div>
-        {props.optionText}
-        <button
+    <div className="option">
+    <p className="option__text">{props.count}. {props.optionText}</p>
+        <button className="button button--link"
             onClick={(e) => {
                 props.handleDeleteOption(props.optionText)
             }}
